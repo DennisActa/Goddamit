d95f78be35d7224c0e0883a658127496012fab2b
DennisActa <assistenzatecnica.actainfo@gmail.com>
2021-09-21 12:38:08 +0200

    update

--- blogapi/src/index.js
+++ blogapi/src/index.js
@@ -25,13 +25,8 @@ export default function Routing() {
         try {
           // Get from local storage by key
           const item = localStorage.getItem(key);
-          // Parse stored json or if none return initialValue
-          if(key === 'access_token') {
-            return item ? true : initialValue;
-          }
-          else {
-            return item ? JSON.parse(item) : initialValue;
-          }
+          // Parse stored json or if none return initialValue          
+          return item ? JSON.parse(item) : initialValue;          
         } catch (error) {
           // If error also return initialValue
           console.log(error);
@@ -40,22 +35,22 @@ export default function Routing() {
       });
       // Return a wrapped version of useState's setter function that ...
       // ... persists the new value to localStorage.
-      // const setValue = (value) => {
-      //   try {
-      //     // Allow value to be a function so we have same API as useState
-      //     const valueToStore =
-      //       value instanceof Function ? value(storedValue) : value;
-      //     // Save state
-      //     setStoredValue(valueToStore);
-      //     // Save to local storage
-      //     localStorage.setItem(key, JSON.stringify(valueToStore));
-      //   } catch (error) {
-      //     // A more advanced implementation would handle the error case
-      //     console.log(error);
-      //   }
-      // };
+      const setValue = (value) => {
+        try {
+          // Allow value to be a function so we have same API as useState
+          const valueToStore =
+            value instanceof Function ? value(storedValue) : value;
+          // Save state
+          setStoredValue(valueToStore);
+          // Save to local storage
+          localStorage.setItem(key, JSON.stringify(valueToStore));
+        } catch (error) {
+          // A more advanced implementation would handle the error case
+          console.log(error);
+        }
+      };
 
-      return [storedValue, setStoredValue];
+      return [storedValue, setValue];
     }
 
     return (
